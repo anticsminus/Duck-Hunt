6d6b62e9b9ecd4b3c0e7c5b840889483ae062f78
AnticsMinus <Antics_chris@hotmail.com>
2019-05-22 21:11:08 -0400

    Pause menu implementation
    Added pause menu to the game

--- Duck Hunt/Assets/Duck Hunt/_Scripts/Manager Scripts/UIManager.cs
+++ Duck Hunt/Assets/Duck Hunt/_Scripts/Manager Scripts/UIManager.cs
@@ -6,9 +6,16 @@ public class UIManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public static UIManager instance = null;
+
+    [Header("Pause Menu Assignment")]
+    [Tooltip("Assign the pause menu prefab here to toggle/untoggle the pause menu based upon pressing escape")]
+    public GameObject PauseMenu;
+    private bool Paused;
+    private float CurentTime;
     // Start is called before the first frame update
     void Awake()
     {
+        Paused = false;
         if (instance == null)
         {
             DontDestroyOnLoad(gameObject);
@@ -18,5 +25,27 @@ public class UIManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    public void Resume()
+    {
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    private void Update()
+    {
+        CurentTime = Time.time;
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Paused = !Paused;
+            PauseMenu.SetActive(Paused);
+        }
+        if (Paused == true)
+        {
+            Time.timeScale = 0;
+        }
+        else
+            Time.timeScale = 1;
     }
 }
